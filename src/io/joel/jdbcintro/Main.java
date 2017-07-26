724de3a3782bbb12ead793f5ace37f0f7a71b11c
taddeimania <jtaddei@gmail.com>
2017-07-26 09:39:51 -0400

    Created boilerplate connection to sqlite db

--- src/io/joel/jdbcintro/Main.java
+++ src/io/joel/jdbcintro/Main.java
@@ -1,8 +1,16 @@
 package io.joel.jdbcintro;
 
+import java.sql.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class.forName("org.sqlite.JDBC");
+
+        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:stats.db")) {
+
+        } catch (SQLException ex) {
+            System.out.println("Something went wrong with your DB connection.");
+        }
     }
 }
