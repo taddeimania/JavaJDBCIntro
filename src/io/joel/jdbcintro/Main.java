3d21121392ca9d550e00951a813fa0dc9a3ec9a5
taddeimania <jtaddei@gmail.com>
2017-07-26 10:01:52 -0400

    Done with create and read operations

--- src/io/joel/jdbcintro/Main.java
+++ src/io/joel/jdbcintro/Main.java
@@ -11,6 +11,16 @@ public class Main {
             Statement statement = connection.createStatement();
             statement.executeUpdate("DROP TABLE IF EXISTS stats");
             statement.executeUpdate("CREATE TABLE stats (id INTEGER PRIMARY KEY, name STRING, wins INTEGER, losses INTEGER)");
+            statement.executeUpdate("INSERT INTO stats (name, wins, losses) VALUES ('Joel', 10, 2)");
+            ResultSet rs = statement.executeQuery("SELECT * FROM stats");
+
+            while (rs.next()) {
+                String name = rs.getString("name");
+                int wins = rs.getInt("wins");
+                int losses = rs.getInt("losses");
+                System.out.printf("%s %s %s", name, wins, losses);
+            }
+
         } catch (SQLException ex) {
             System.out.println("Something went wrong with your DB connection.");
         }
